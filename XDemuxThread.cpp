9fcc288f0ed98dcb484f6ebf953698b064917c89
lgh <2524398864@qq.com>
2022-10-31 14:48:01 +0800

    feat: 实现XDemuxThread类的open接口

--- XDemuxThread.cpp
+++ XDemuxThread.cpp
@@ -1,4 +1,10 @@
 #include "XDemuxThread.h"
+#include "XDemux.h"
+#include "XVideoThread.h"
+#include "XAudioThread.h"
+
+#include <iostream>
+using namespace std;
 
 XDemuxThread::XDemuxThread()
 {
@@ -12,5 +18,40 @@ XDemuxThread::~XDemuxThread()
 
 bool XDemuxThread::Open(const char *url, IVideoCall *call)
 {
+    // 容错处理
+    if(url == 0 || url[0] == '\0')
+        return false;
 
+    mux.lock();
+
+    if(!demux) demux = new XDemux();
+    if(!vt) vt = new XVideoThread();
+    if(!at) at = new XAudioThread();
+
+    // 打开解封装
+    bool ret = demux->Open(url);
+    if(!ret)
+    {
+        mux.unlock();
+        cout << "demux->Open(url) failed!" << endl;
+        return false;
+    }
+
+    // 打开视频解码器和处理线程
+    if(!vt->Open(demux->CopyVPara(), call, demux->width, demux->height))
+    {
+        ret = false;
+        cout << "vt->Open() failed!" << endl;
+    }
+
+    // 打开音频解码器和处理线程
+    if(!at->Open(demux->CopyAPara()))
+    {
+        ret = false;
+        cout << "at->Open() failed!" << endl;
+    }
+
+    mux.unlock();
+    cout << "XDemucThread::Open seccess!" << endl;
+    return ret;
 }
--- XDemuxThread.h
+++ XDemuxThread.h
@@ -2,15 +2,25 @@
 #define XDEMUXTHREAD_H
 
 #include <QThread>
+#include <mutex>
 #include "IVideoCall.h"
+
+class XDemux;
+class XVideoThread;
+class XAudioThread;
 
 class XDemuxThread: public QThread
 {
 public:
     XDemuxThread();
     ~XDemuxThread();
-    bool Open(const char *url, IVideoCall *call);
+     virtual bool Open(const char *url, IVideoCall *call);
 
+protected:
+    std::mutex mux;
+    XDemux *demux = 0;
+    XVideoThread *vt = 0;
+    XAudioThread *at = 0;
 };
 
 #endif // XDEMUXTHREAD_H
